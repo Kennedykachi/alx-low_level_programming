ecd5c4b6e1dec6fb22f0d8f1818d17b85be0749d
Kennedykachi <kennedyonyekachi@gmail.com>
2022-12-12 10:10:08 -0800

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,10 +4,7 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-
 int main(void)
-
-
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
