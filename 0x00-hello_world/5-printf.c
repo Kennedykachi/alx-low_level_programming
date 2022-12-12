aa00cb97c5e295b9ecd44327666d150f0970a767
Kennedykachi <kennedyonyekachi@gmail.com>
2022-12-12 07:29:44 -0800

    Task 4 and 5 corrections

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,12 @@
 
 /**
 main - Entry Point
-
 Return: Always 0 (Success)
 */
 
 int main(void)
 {
-     printf("with proper grammar, but the outcome is a piece of art\n");
-     return (0);
+printf("with proper grammar, but the outcome is a piece of art");
+return (0);
 
 }
