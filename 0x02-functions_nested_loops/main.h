969978e24452fb6a8fadb5ecd3424229003ccf65
Kennedykachi <kennedyonyekachi@gmail.com>
2022-12-14 15:34:48 -0800

    function that checks for lower character

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,14 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
 
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+void num(int r, int n);
+
+#endif
